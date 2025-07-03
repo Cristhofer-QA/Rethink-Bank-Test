10f03a2d93a1584baff9ca0fd249caeeb893504d
Cristhofer Mian <cristhofer.mian@e-stracta.com>
2025-07-03 18:07:22 -0300

    fix: corrige código

--- checker/status/generalBalanceStatusCheck.js
+++ checker/status/generalBalanceStatusCheck.js
@@ -2,7 +2,7 @@ const successGeneralBalance = 200;
 
 function checkGeneralBalanceSuccess(response) {
     expect(response.status).toBe(successGeneralBalance);
-};s
+};
 
 module.exports = { 
     checkGeneralBalanceSuccess,
